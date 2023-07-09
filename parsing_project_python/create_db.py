e8f844111e39c3cbecc32da144d14034ea594253
Egor <egor.mokievsky@gmail.com>
2023-07-09 19:48:03 +0300

    slightly increased the size of some fields

--- parsing_project_python/create_db.py
+++ parsing_project_python/create_db.py
@@ -17,7 +17,16 @@ def create_tables(cur, conn):
                         FOREIGN KEY(id_article) REFERENCES articles(id))""")
     conn.commit()
 
+def alter_table(cur, conn):
+    cur.execute("""ALTER TABLE content_articles 
+                        MODIFY COLUMN id_article VARCHAR(50)"""
+                )
+    cur.execute("""ALTER TABLE articles
+                        MODIFY COLUMN id VARCHAR(50)
+                        MODIFY COLUMN reference VARCHAR(300)""")
+    conn.commit()
+
 with pymysql.connect(db='first_database', user='root', 
                       host='localhost', password='*************') as conn:
     cur = conn.cursor()
-    create_tables(cur, conn)
+    alter_table(cur, conn)
